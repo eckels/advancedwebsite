b16b63e943969b8e2966bebfe9b54285d996e5c3
Evan Eckels <eveckels@gmail.com>
2018-01-20 15:19:03 -0600

    final changes

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getText() {
   var bobtext = document.getElementById('mytext').value;
   console.log(bobtext);
+  var i = 0;
+  while (i < 7) {
+    var temp = bobtext.charAt(i);
+    i++;
+    var myid = 'p' + i;
+    document.getElementById(myid).innerHTML = temp;
+  }
 }
